6e2de28dbdbd99f1140e97b7ce443e0617af7bdb
znddzxx112 <znddzxx112@163.com>
2020-03-08 23:48:43 +0800

    install ubuntu

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker build centos7_nginx/. -t centos7nginx
+docker build ubuntu1804_nginx/. -t ubuntu1804nginx
 
-docker build centos7_nginx_php/. -t centos7nginxphp
+docker build ubuntu1804_nginx_php/. -t ubuntu1804nginxphp
 
-docker build centos7_nginx_php_prod/. -t centos7nginxphpprod
+docker build ubuntu1804_nginx_php_prod/. -t ubuntu1804nginxphpprod
 
